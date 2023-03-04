cb911978ccc397ef16848ee6f46c66e910c28bf6
omar <omarpeart100@gmail.com>
2023-03-03 21:21:53 -0500

    updated cart to redux somewhat

---
+++ src/store/cart/cart.action.jsx
@@ -0,0 +1,47 @@
+import { CART_ACTION_TYPES } from "./cart.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+const removeCartItem = (cartItems, productToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === productToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+const removeItem = (cartItems, itemToRemove) => {
+  return cartItems.filter((cartItem) => cartItem.id !== itemToRemove.id);
+};
+export const setIsCartOpen = (boolean) =>
+  createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
+const addItemToCart = (cartItems, productToAdd) => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  updateCartItemsReducer(newCartItems);
+};
+const removeItemFromCart = (cartItems, productToRemove) => {
+  const newCartItems = removeCartItem(cartItems, productToRemove);
+  updateCartItemsReducer(newCartItems);
+};
+const removeItems = (cartItems, itemToRemove) => {
+  const newCartItems = removeItem(cartItems, itemToRemove);
+  updateCartItemsReducer(newCartItems);
+};
